439dc52a76851de027485a993693253aeab8ade4
Evgeniy <46program@gmail.com>
2023-08-04 23:59:15 +0300

    add logout button

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/service/cart.service';
+import {TokenStorageService} from "../../service/token-storage.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -10,7 +12,9 @@ export class HeaderComponent implements OnInit {
 
   public totalItem : number = 0;
   public searchTerm !: string;
-  constructor(private cartService : CartService) { }
+  constructor(private cartService : CartService,
+              private tokenService : TokenStorageService,
+              private router:Router) { }
 
   ngOnInit(): void {
     this.cartService.getProducts()
@@ -24,5 +28,10 @@ export class HeaderComponent implements OnInit {
     this.searchTerm = (event.target as HTMLInputElement).value;
     console.log(this.searchTerm);
     this.cartService.search.next(this.searchTerm);
+  }
+
+  logout() {
+    this.tokenService.logOut();
+    this.router.navigate(['/login']);
   }
 }
